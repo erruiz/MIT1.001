10ca496b7e6de4a5cc6bee95660476137352f675
erruiz <eruiz@g.harvard.edu>
2017-03-03 00:19:02 -0500

    erruiz added new files

---
+++ practice.js
@@ -0,0 +1,16 @@
+//practice
+
+var a = [1,2,3,4];
+
+//findMax with a for loop
+var findMax = function(b){
+    var max = 0;
+    for(var i = 0;i<b.length,i++){
+        if(b[i]>max){
+            max = b[i];
+        }
+    };
+    return max;
+};
+
+console.log(max);
